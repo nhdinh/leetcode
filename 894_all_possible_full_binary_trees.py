1e0630a76560da91aecf227fd27b34570a175556
Hung Dinh <hung.dnc@gmail.com>
2022-07-12 09:43:56 +0700

    894

--- 894_all_possible_full_binary_trees.py
+++ 894_all_possible_full_binary_trees.py
@@ -1,8 +1,7 @@
-from operator import le
+from hashlib import new
+from tkinter.tix import Tree
 from typing import List, Optional
 from copy import copy
-
-# Definition for a binary tree node.
 
 
 class TreeNode:
@@ -11,50 +10,54 @@ class TreeNode:
         self.left = left
         self.right = right
 
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, TreeNode):
+            return False
+
+        return self.val == __o.val and self.left == __o.left and self.right == __o.right
+
 
 class Solution:
-    def allPossibleFBT(self, n: int, nodes={}) -> List[Optional[TreeNode]]:
-        def getLeaf(_node: TreeNode, _leaves=[]):
-            if _node is None:
-                return None
-            if _node.left is not None:
-                _leaves.append(getLeaf(_node.left, _leaves))
-            if _node.right is not None:
-                _leaves.append(getLeaf(_node.right, _leaves))
-            if _node.left is None and _node.right is None:
-                _leaves.append(_node)
-
-            return _leaves
-
+    def allPossibleFBT(self, n: int, mem={1: [TreeNode()]}) -> List[Optional[TreeNode]]:
         if n % 2 == 0:
             return []
-        elif n == 1:
-            return [TreeNode()]
         else:
-            if n not in nodes:
-                nodes[n] = []
-                prev_node = self.allPossibleFBT(n-2, nodes)
+            if n not in mem:
+                ans = []
+                for i in range(1, n, 2):
+                    j = n - i - 1
+                    for all_left in self.allPossibleFBT(i):
+                        for all_right in self.allPossibleFBT(j):
+                            t = TreeNode()
+                            t.left = all_left
+                            t.right = all_right
+                            ans.append(t)
+
+                mem[n] = ans
+
+            return mem[n]
 
 
 if __name__ == '__main__':
     ss = Solution()
 
-    # print([TreeNode()])
+    s3 = ss.allPossibleFBT(3)
+    e3 = [TreeNode(0, TreeNode(), TreeNode())]
+    assert e3 == s3
 
-    # t0 = TreeNode()
-    # t0.left = TreeNode()
-    # t0.right = TreeNode()
-    # t0.right.left = TreeNode()
-    # t0.right.right = TreeNode()
-    # t0.right.right.left = TreeNode()
-    # t0.right.right.right = TreeNode()
-    # print([t0])
+    s7 = ss.allPossibleFBT(7)
+    e7 = [
+        TreeNode(0, TreeNode(0, TreeNode(), TreeNode()),
+                 TreeNode(0, TreeNode(), TreeNode())),
+        TreeNode(0, TreeNode(0, TreeNode(0, TreeNode(),
+                 TreeNode()), TreeNode()), TreeNode()),
+        TreeNode(0, TreeNode(0, TreeNode(), TreeNode(
+            0, TreeNode(), TreeNode())), TreeNode()),
+        TreeNode(0, TreeNode(), TreeNode(0, TreeNode(),
+                 TreeNode(0, TreeNode(), TreeNode()))),
+        TreeNode(0, TreeNode(), TreeNode(0, TreeNode(
+            0, TreeNode(), TreeNode()), TreeNode()))
+    ]
 
-    # s0 = ss.allPossibleFBT(1)
-    # print(set(s0) == set([[0]]))
-
-    s2 = ss.allPossibleFBT(3)
-    # print(set(s2)==set([[0,0,0]]))
-
-    # s1 = ss.allPossibleFBT(7)
-    # print(set(s1)==set([[0,0,0,None,None,0,0,None,None,0,0],[0,0,0,None,None,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,None,None,None,None,0,0],[0,0,0,0,0,None,None,0,0]]))
+    assert all(i7 in e7 for i7 in s7)
+    assert all(i7 in s7 for i7 in e7)
